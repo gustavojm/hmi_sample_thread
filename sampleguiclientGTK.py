b801497ee8f84c3a1431981d6ce6fd9cf185313e
Gustavo Malano <gustavojmalano@gmail.com>
2021-04-06 01:11:26 -0300

    Se debe hacer strip del caracter \0 en el string recibido desde la red sino el parseo del JSON da error.

--- sampleguiclientGTK.py
+++ sampleguiclientGTK.py
@@ -12,7 +12,9 @@ from socketclientthread import SocketClientThread, ClientCommand, ClientReply
 
 SERVER_ADDR = '192.168.1.12', 1234
 
+
 class SampleGUIClientWindow():
+
     def __init__(self, parent=None):
         self.create_client()
 
@@ -37,7 +39,6 @@ class SampleGUIClientWindow():
         self.client.cmd_q.put(ClientCommand(ClientCommand.CONNECT, SERVER_ADDR))        
         self.create_timers()
         self.window.show()
-        
 
     def create_client(self):
         self.client = SocketClientThread()
@@ -48,12 +49,11 @@ class SampleGUIClientWindow():
         self.client_reply_timer = GObject.timeout_add(10, self.on_client_reply_timer)
         self.telemetry_timer = GObject.timeout_add(100, self.telemetria)
 
-
     def on_btn_switch_state_set(self, widget, state):
         return True
         
     def on_btn_switch_button_press_event(self, widget, event):
-        #self.client.cmd_q.put(ClientCommand(ClientCommand.CONNECT, SERVER_ADDR))
+        # self.client.cmd_q.put(ClientCommand(ClientCommand.CONNECT, SERVER_ADDR))
         
         commands_obj = {
           "commands": [
@@ -64,30 +64,32 @@ class SampleGUIClientWindow():
         
         self.client.cmd_q.put(ClientCommand(ClientCommand.SEND, json.dumps(commands_obj)))
         self.client.cmd_q.put(ClientCommand(ClientCommand.RECEIVE))
-        #self.client.cmd_q.put(ClientCommand(ClientCommand.CLOSE))
+        # self.client.cmd_q.put(ClientCommand(ClientCommand.CLOSE))
         
     def doit(self, widget, event):
         self.telemetria()
 
     def telemetria(self):
-        #self.client.cmd_q.put(ClientCommand(ClientCommand.CONNECT, SERVER_ADDR))        
+        # self.client.cmd_q.put(ClientCommand(ClientCommand.CONNECT, SERVER_ADDR))        
         commands_obj = {
           "commands": [
             {"command": "CONTROL_ENABLE"
             },
             {"command": "LOGS", "pars": {
-                        "quantity": 10 
+                        "quantity": 10
                         }    
             },
             {"command": "TELEMETRIA"
             },
+            {"command": "PROTOCOL_VERSION"
+            }
           ]
         }
         
         self.client.cmd_q.put(ClientCommand(ClientCommand.SEND, json.dumps(commands_obj)))
         self.client.cmd_q.put(ClientCommand(ClientCommand.RECEIVE))
-        #self.client.cmd_q.put(ClientCommand(ClientCommand.CLOSE))
-        return True         #to allow repetition of the timeout
+        # self.client.cmd_q.put(ClientCommand(ClientCommand.CLOSE))
+        return True  # to allow repetition of the timeout
 
     def on_client_reply_timer(self):
         try:
@@ -103,27 +105,28 @@ class SampleGUIClientWindow():
 
     def log(self, msg):
         timestamp = '[%010.3f]' % time.clock()
-        print(str(msg))
-        end_iter = self.logs_buffer.get_end_iter()
-        self.logs_buffer.insert(end_iter, str(msg))
+        # end_iter = self.logs_buffer.get_end_iter()
+        # self.logs_buffer.insert(end_iter, str(msg))
         
-        print(str(msg))
-        
-    def update_ui(self, data):
-        objeto_json = json.loads(str(data))                    
-
-        try:        
-            self.btn_switch.set_state(objeto_json["CONTROL_ENABLE_CMD"]["ACK"])
-            self.btn_switch.set_active(self.btn_switch.get_state())
-
-            for log in objeto_json["LOGS"]["DEBUG_MSGS"]:
-                print(log)
-
-            self.lbl_posActArm.set_text(str(objeto_json["TELEMETRIA"]["ARM"]["posAct"]))
-            print("Stalled ARM: %s" % objeto_json["TELEMETRIA"]["ARM"]["stalled"])                    
-            print("Posición actual POLE: %i" % objeto_json["TELEMETRIA"]["POLE"]["posAct"])
-        except KeyError:
-            print("no encontrado")
+    def update_ui(self, data): 
+        try: 
+            objeto_json = json.loads(str(data).rstrip('\0'))                    
+    
+            try: 
+                self.btn_switch.set_state(objeto_json["CONTROL_ENABLE"]["ACK"])
+                self.btn_switch.set_active(self.btn_switch.get_state())
+    
+                for log in objeto_json["LOGS"]["DEBUG_MSGS"]:
+                    print(log)
+    
+                self.lbl_posActArm.set_text(str(objeto_json["TELEMETRIA"]["ARM"]["posAct"]))
+                print("Stalled ARM: %s" % objeto_json["TELEMETRIA"]["ARM"]["stalled"])                    
+                print("Posición actual POLE: %i" % objeto_json["TELEMETRIA"]["POLE"]["posAct"])
+                print("Protocol Version: %s" % objeto_json["PROTOCOL_VERSION"]["Version"])
+            except KeyError:
+                print("no encontrado")
+        except ValueError as e:
+            print("Error decodificando JSON")
 
 
 #-------------------------------------------------------------------------------
